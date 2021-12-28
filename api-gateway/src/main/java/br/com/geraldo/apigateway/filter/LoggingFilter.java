9464bcd246acd6e6cf2930d86fba51a43baf5d85
Geraldo Costa <geraldocoosta@gmail.com>
2021-12-28 19:23:03 -0300

    criando filtro e rotas com apiGateway
    Toda requisicao que entram na minha rede de microservicos bate aqui

---
+++ api-gateway/src/main/java/br/com/geraldo/apigateway/filter/LoggingFilter.java
@@ -0,0 +1,21 @@
+package br.com.geraldo.apigateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import lombok.extern.log4j.Log4j2;
+import reactor.core.publisher.Mono;
+
+@Log4j2
+@Component
+public class LoggingFilter implements GlobalFilter{
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		log.info("Original request path {}", exchange.getRequest().getPath());
+		return chain.filter(exchange);
+	}
+
+}
