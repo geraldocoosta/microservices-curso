58e9df2431cf48cf77bd8e4a02766178d999d9a1
Geraldo Costa <geraldocoosta@gmail.com>
2021-12-29 22:37:25 -0300

    verificando metodos principais do fault-tolerance

--- book-service/src/main/java/br/com/geraldo/bookservice/controllers/FooBarController.java
+++ book-service/src/main/java/br/com/geraldo/bookservice/controllers/FooBarController.java
@@ -3,11 +3,17 @@ package br.com.geraldo.bookservice.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.log4j.Log4j2;
 
+
+/**
+ * 
+ * @author geraldo
+ *
+ * Classe para testar fault-tolerance
+ * mais infos: https://resilience4j.readme.io/docs/getting-started-3
+ */
 @RestController
 @RequestMapping("/book-service")
 @Log4j2
@@ -15,13 +21,52 @@ public class FooBarController {
 
 	private static final String FOO_BAR_ERROR = "Foo-bar Error!";
 
-	@GetMapping("/foo-bar")
+	
 //	@Retry(name = "default") // Tenta realizar a requisição 2 vezes, instancia default -> ver application.yml
-	@Retry(name = "foo-bar", fallbackMethod = "errorInFooBar") // aqui vamos tentar 5 vezes
+//	@Retry(name = "foo-bar", fallbackMethod = "errorInFooBar") // aqui vamos tentar 5 vezes
+
+//	@CircuitBreaker(name = "default", fallbackMethod = "errorInFooBar")
+	// serve para, quando acontecer um erro, em vez de ficar tentando novamente 
+	// infinitas vezes, o @CircuitBreaker manda direto pro fallbackMethod
+	// funciona em 3 estados: 
+	// 	fechado     -> onde receber todas requisições
+	// 	aberto      -> onde todas requisições vão para o fallbackMethod
+	//  meio-aberto -> algumas chamadas vezes, ele vai tentar rodar o metodo normal
+	//				   até ir elevando sua taxa de sucesso para ir para o estado fechado
+	//				   se continuar no erro, volta pro aberto
+
+//	@RateLimiter(name = "default") // , fallbackMethod = "errorInFooBar")
+	// determina quantas requisições esse endpoint pode
+	// receber em um perido de tempo, ex: 10000 chamadas a cada segundo 10 segundos
+	// configuravel (assim como todas features de fault-tolerance) no application.yml
+	// também aceita fullbackMethod
+	
+//	@Bulkhead(name = "default")  // , fallbackMethod = "errorInFooBar")
+	// determina quantas execuções concorrentes vou ter
+	// Mais informações, ver application.yml
+	
+//	@TimeLimiter(name = "default") // , fallbackMethod = "errorInFooBar")
+	// define um tempo para timeout, configuravel pelo application.yml
+	// Só funciona se o seu método retornar um Future ou seja reativo (Flux or Mono)
+	
+	@GetMapping("/foo-bar")
 	public String fooBar() {
 		log.info("Request to foo-bar is received!");
-		var response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
-		return response.getBody();
+		
+		// Comentado, parte utilizada para testar TimeLimit
+		try {
+			Thread.sleep(3000);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		
+		// Comentado, parte utilizada para testar Retry e CircuitBreaker
+		/*
+		 * var response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
+		 * return response.getBody();
+		 */
+		 
+		return "Foo-Bar";
 	}
 	
 	// Método com o mesmo nome do fallbackMethod em cima mencionado (na annotation @Retry)
