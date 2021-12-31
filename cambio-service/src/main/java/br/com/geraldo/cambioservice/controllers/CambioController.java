25576c5eb16ed88db34230d3b1396af8bd425f36
Geraldo Costa <geraldocoosta@gmail.com>
2021-12-31 15:46:22 -0300

    log para mostrar id da req q o zipkin utiliza

--- cambio-service/src/main/java/br/com/geraldo/cambioservice/controllers/CambioController.java
+++ cambio-service/src/main/java/br/com/geraldo/cambioservice/controllers/CambioController.java
@@ -12,22 +12,21 @@ import br.com.geraldo.cambioservice.services.CambioService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
 @Tag(name = "Cambio Service Api")
 @RestController
 @RequestMapping("/cambio-service")
 @RequiredArgsConstructor
+@Log4j2
 public class CambioController {
-	
+
 	private final CambioService cambioService;
-	
+
 	@Operation(summary = "Find the value of a currency in another country")
 	@GetMapping("/{amount}/{from}/{to}")
-	public Cambio getCambio(
-			@PathVariable BigDecimal amount,
-			@PathVariable String from,
-			@PathVariable String to) {
-		
+	public Cambio getCambio(@PathVariable BigDecimal amount, @PathVariable String from, @PathVariable String to) {
+		log.info("getCambio is called with -> {} {} {}", amount, from, to);
 		return cambioService.getCambio(amount, from, to);
 	}
 
